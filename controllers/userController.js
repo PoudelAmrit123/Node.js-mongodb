dd0aa070653e1cc9198163cfeb5bae3d224ea354
Amrit Poudel <amritpoudel433@gmail.com>
2023-02-22 23:56:46 +0545

    Update the User Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,20 @@
 const User = require('./../models/userModel');
 const catchAsync = require("../utils/catchAsync");
+const AppError = require('../utils/appError');
+const { findByIdAndUpdate } = require('./../models/userModel');
+
+
+
+   const filterObj = (obj , ...allowedFields) =>{
+    const newObj = {} ;
+    Object.keys(obj).forEach( el => {
+      if ( allowedFields.includes(el)) newObj[el] = obj[el]; 
+    });
+    return newObj ;
+   }
+
+
+
 
 exports.getAllUsers = catchAsync( async (req, res , next) => {
  
@@ -12,6 +27,38 @@ exports.getAllUsers = catchAsync( async (req, res , next) => {
      }
   });
 });
+
+
+ exports.updateMe = async ( req , res , next)=>{
+
+      //  1 ) sending error if some one try to update the password 
+         if(req.body.password || req.body.passwordConfirm ){
+          return next( new AppError('This is not the route for changing the password' , 401));
+         }
+
+      //  2) Sending the documention 
+ 
+      //  we donot use User.save() bcz we donot want password and passwordConfirm so better idea is to use findByIdAndUpdate 
+ 
+      const filteredBody = filterObj(req.body , 'name' , 'email');
+       //Here filterObj is function which return the filtered Object 
+
+        const updatedUser = await User.findByIdAndUpdate(req.user.id , filteredBody , { 
+          new : true ,
+          runValidators : true ,
+        })
+        //Here we find the id by using findbyidandUpdate and filterdobj are those obj where we want to update basically not password , role etc and update it 
+
+
+              res.status(200).json({
+                status : 'success' ,
+                data : {
+                  user : updatedUser , 
+                } 
+
+              })
+   
+ }
 
 
 exports.getUser = (req, res) => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.post('/login' , authController.login);
 router.post('/forgetPassword' , authController.forgetPassword);
 router.patch('/resetPassword:token' , authController.resetPassword);
 router.patch('/updateMyPassword' , authController.protect , authController.updatePassword);
+router.patch('/updateMe' , authController.protect , userController.updateMe);
 
 router
   .route('/')
