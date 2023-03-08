46b759a1f0fbd317d3283228c90a83db77488e59
Amrit Poudel <amritpoudel433@gmail.com>
2023-03-09 00:03:54 +0545

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,46 +1,54 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 
-const reviewSchema =  new mongoose.Schema(
-{
-  review : {
-    type : String ,
-    required : ['true' , 'Review cannot be empty'] , 
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: ["true", "Review cannot be empty"],
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+
+    //Parent refrencing
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: ["true", "The review must have an Tour "],
+    },
+
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: ["true", "The review must have an user"],
+    },
   },
-
-  rating : {
-    type : Number ,
-    min : 1 , 
-    max : 5 ,
-  },
-
-  createdAt : {
-    type : Date , 
-    default : Date.now(),
-  },
-
-  //Parent refrencing 
-  tour : {
-    type : mongoose.Schema.ObjectId ,
-    ref : 'Tour',
-    required : ['true' , 'The review must have an Tour '],
-
-  },
-
-  review : {
-    type : mongoose.Schema.ObjectId ,
-    ref : 'User' ,
-    required : ['true' , 'The review must have an user'],
-  },
-  
-  
-
-} , 
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   }
-)
+);
 
-const Review = new mongoose.model('Review' , reviewSchema);
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: "tour",
+    select: "name",
+  }).populate({
+    path: "user",
+    select: "name photo",
+  });
 
-module.exports = Review ;
+  next();
+});
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
