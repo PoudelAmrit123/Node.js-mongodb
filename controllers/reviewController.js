0f8e4b378ce1d378ef636501637049e335c20a68
Amrit Poudel <amritpoudel433@gmail.com>
2023-03-12 00:10:46 +0545

    Nested Get endPoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,9 +2,13 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllReviews = catchAsync( async (req , res , next)=>{
 
-     const reviews = await  Review.find();
+
+
+exports.getAllReviews = catchAsync( async (req , res , next)=>{
+     let filter ={};
+   if(req.params.tourId ) filter = { tour : req.params.tourId} ;
+     const reviews = await  Review.find(filter);
 
 
        res.status(200).json({
